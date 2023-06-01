b0944a435732a342d4850e0b8e7fef6726669ee1
LopataNV <133806234+LopataNV@users.noreply.github.com>
2023-06-01 13:17:58 +0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export function Footer ()
+{
+  const DivStyle = {
+    width: '100%',
+    neight: '750px',
+    background: 'blue',
+    color: 'black',
+    fontSize: '25px',
+  };
+  return (  <h1 style={DivStyle}> Подвал </h1>
+  );
+}
+
+export default Footer;
---
+++ Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export function Main ()
+{
+  const DivStyle = {
+    width: '100%',
+    neight: '750px',
+    background: 'blue',
+    color: 'black',
+    fontSize: '25px',
+  };
+  return (  <h1 style={DivStyle}> Тело </h1>
+  );
+}
+
+export default Main;
